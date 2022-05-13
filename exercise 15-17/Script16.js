9972760a19ee369feadb957c55a6e0eafb4341f4
Joel Laita <98139316+20198338@users.noreply.github.com>
2022-05-13 10:18:53 +0800

    Add files via upload

---
+++ exercise 15-17/Script16.js
@@ -0,0 +1,1 @@
+document.getElementById("Information ").innerHTML="paragrapgh changed.";
---
+++ exercise 15-17/Script17.js
@@ -0,0 +1,8 @@
+document.write
+const mytext='Here is \nsome text!'; 
+ newFunction();
+ console.log(mytext);
+
+function newFunction() {
+    mytext = Today; is; another; busy; day!; ";";
+}
